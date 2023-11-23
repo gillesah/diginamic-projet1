f3962f4e18b486816b2dd4ba55e1d30c1e76269f
Gilles Helleu <helleugilles@gmail.com>
2023-11-23 15:04:11 +0100

    modification des dossiers

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .client import Client
-# from .ouvrage import Ouvrage
-# from .theme import Theme
-# from .commentaire import Commentaire
-# from .theme_ouvrage import ThemeOuvrage
--- src/models/commentaire.py
+++ src/models/commentaire.py
@@ -1,2 +1,14 @@
-class Commentaire:
-    pass
+from config.connexion import Base
+from sqlalchemy import ForeignKey, String, Date
+from sqlalchemy.orm import Mapped, mapped_column
+from client import Client
+from ouvrage import Ouvrage
+
+class Commentaire(Base):
+    __tablename__ = "Commentaire"
+    id_commentaire : Mapped[int] = mapped_column(primarykey=True, autoincrement=True)
+    id_client : Mapped[int] = mapped_column(ForeignKey("Client.id_client)")
+    id_ouvrage : Mapped[int] = mapped_column(ForeignKey("Ouvrage.id_ouvrage"))
+    date_publication_commentaire : Mapped[Date] = mapped_column(Date)
+    auteur_commentaire : Mapped[str] = mapped_column(String(255))
+    titre_commentaire : Mapped[str] = mapped_column(String(255))
