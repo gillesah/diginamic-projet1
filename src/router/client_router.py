b858944c038ac23c3d213c970e2da200da0edc2e
Guillaume Lemaitre <guillaume.lemaitre1@gmail.com>
2023-11-24 13:19:38 +0000

    add a comment

--- src/router/client_router.py
+++ src/router/client_router.py
@@ -10,6 +10,7 @@ client_router = APIRouter(tags=["client"], prefix="/client")
 
 @client_router.post("/add_client", response_model=ClientSchemaOut, summary="Ajoute les informations pour un client", status_code=status.HTTP_201_CREATED)
 def add_client(client: ClientSchema, db: Session = Depends(get_db)):
+    """Permet d'ajouter les informations pour un nouveau client"""
     client_db = Client(**client.dict())
     db.add(client_db)
     db.commit()
