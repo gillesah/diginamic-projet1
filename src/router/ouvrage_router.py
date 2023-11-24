53db81fa510882c378a2dda7b529114be017512d
Gilles Helleu <helleugilles@gmail.com>
2023-11-24 16:26:46 +0100

    modification de put ouvrage qui marche

--- src/router/ouvrage_router.py
+++ src/router/ouvrage_router.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import Session
 from config.connexion import Session, get_db
 from src.models.ouvrage import Ouvrage, OuvrageCreate, OuvrageUpdate
 from fastapi.encoders import jsonable_encoder
+from typing import List
 
 app = FastAPI()
 ouvrage_router = APIRouter()
 
-
+# POST : création d'un ouvrage
 @ouvrage_router.post("/ouvrages/", status_code=status.HTTP_201_CREATED)
 def create_ouvrage(ouvrage: OuvrageCreate, db: Session = Depends(get_db)):
     db_ouvrage = Ouvrage(**ouvrage.dict())
@@ -17,7 +18,7 @@ def create_ouvrage(ouvrage: OuvrageCreate, db: Session = Depends(get_db)):
     db.close()
     return db_ouvrage
 
-
+# DELETE : suppression d'un ouvrage
 @ouvrage_router.delete("/ouvrages/{id_ouvrage}", status_code=status.HTTP_201_CREATED)
 def delete_ouvrage(id_ouvrage: int, db: Session = Depends(get_db)):
     db_ouvrage = db.get(Ouvrage, id_ouvrage)
@@ -30,23 +31,19 @@ def delete_ouvrage(id_ouvrage: int, db: Session = Depends(get_db)):
         db.close()
         raise HTTPException(status_code=404, detail="ouvrage pas trouvé")
 
-# PUT : mise à jour d'un ITEM
 
-
-# @ouvrage_router.put("/ouvrages/{id_ouvrage}", response_model=OuvrageUpdate, status_code=status.HTTP_200_OK)
-# async def update_ouvrage(id_ouvrage: int, ouvrage_update: OuvrageUpdate, db: Session = Depends(get_db)):
-#     db_ouvrage = db.get(Ouvrage, id_ouvrage)
-#     if OuvrageUpdate.id == id_ouvrage:
-#         updated_ouvrage = ouvrage_update(
-#             id=id_ouvrage, **OuvrageUpdate.dict())
-#         db_ouvrage[db_ouvrage.index(id_ouvrage)] = updated_ouvrage
-#         return updated_ouvrage
-#     raise HTTPException(
-#         status_code=404, detail="L'ouvrage n'est pas trouvé")
-
-
+# PUT : mise à jour d'un Ouvrage
 @ouvrage_router.put("/ouvrages/{id_ouvrage}", response_model=OuvrageUpdate, status_code=status.HTTP_200_OK)
 async def update_ouvrage(id_ouvrage: int, ouvrage_update: OuvrageUpdate, db: Session = Depends(get_db)):
-    update_item_encoded = jsonable_encoder(ouvrage_update)
-    Ouvrage[id_ouvrage] = update_item_encoded
-    return update_item_encoded
+    db_ouvrage = db.get(Ouvrage, id_ouvrage) or None
+    if db_ouvrage is not None:
+        for key, value in ouvrage_update.dict().items():
+            # attribuer les nouvelles valeurs avec setattr
+            setattr(db_ouvrage, key, value)
+        db.commit()
+        return db_ouvrage
+    raise HTTPException(
+        status_code=404, detail="L'ouvrage n'est pas trouvé")
+
+
+# PATH
