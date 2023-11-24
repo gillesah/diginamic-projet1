b1693cc06be45c127e3fbecfac0419d75e83765a
Benjamin Hadjaz <146709484+0bshidian@users.noreply.github.com>
2023-11-24 16:00:50 +0100

    code commentaires routes avant tests

--- src/router/commentaire_router.py
+++ src/router/commentaire_router.py
@@ -7,8 +7,9 @@ from src.schema.commentaire_schema import Commentaire_create, CommentaireRespons
 app = FastAPI()
 commentaire_router = APIRouter()
 
-@app.post("/commentaire/", response_model= CommentaireResponse, tags = ["commentaires"])
-async def create_Commentaire(commentaire : Commentaire_create,  db: Session = Depends(get_db)):
+@app.post("/commentaires/", response_model= CommentaireResponse, tags = ["commentaires"], status_code=status.HTTP_201_CREATED, summary="création d'une occurence.")
+async def create_Commentaire(commentaire : Commentaire_create):
+    db: Session = Depends(get_db)
     db_comment = Commentaire(**commentaire.dict())
     db.add(db_comment)
     db.commit()
@@ -16,36 +17,44 @@ async def create_Commentaire(commentaire : Commentaire_create,  db: Session = De
     db.close()
     return db_comment
 
-@app.get("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaires"])
+@app.get("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaires"], status_code=status.HTTP_200_OK, summary="visualisation d'une occurence d'apres un id.")
 async def read_commentaire(commentaire_id : int):
-    for comment in TableCommentaire:
-        if comment.id_commentaire == commentaire_id:
-            return {f"Titre : {comment.titre_commentaire}, commentaire : {comment.auteur_commentaire}, publié le {comment.date_publication_commentaire}."}
-    raise HTTPException(status_code=404, detail="Comment not found")
+    db: Session = Depends(get_db)
+    db_commentaire = db.get(Commentaire, commentaire_id)
+    if db_commentaire:
+            return {f"Titre : {db_commentaire.titre_commentaire}, commentaire : {db_commentaire.auteur_commentaire}, publié le {db_commentaire.date_publication_commentaire}."}
+    else : 
+        raise HTTPException(status_code=404, detail="Comment not found.")
 
-@app.put("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaire"])
+@app.put("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaire"], status_code=status.HTTP_200_OK, summary="modification complete d'une occurence.")
 async def update_item(commentaire_id : int, commentUpdate : CommentaireUpdate):
-    for comment in TableCommentaire:
-        if comment.id_commentaire == commentaire_id:
-            update_item = CommentaireResponse(id=commentaire_id, auteur_commentaire=commentUpdate.auteur_commentaire, titre_commentaire=commentUpdate.titre_commentaire)
-            TableCommentaire[TableCommentaire.index(comment)] = update_item
-            return update_item
-    raise HTTPException(status_code=404, detail="Comment not found")
+    db: Session = Depends(get_db)
+    db_commentaire = db.get(Commentaire, commentaire_id)
+    if db_commentaire:
+        updated_comment = CommentaireResponse(**commentUpdate.dict())
+        db_commentaire[db_commentaire.index(commentaire_id)] = updated_comment
+        return updated_comment
+    else: 
+        raise HTTPException(status_code=404, detail="Comment not found.")
 
-@app.patch("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaire"], status_code=status.HTTP_200_OK, summary="modification partielle")
+@app.patch("/commentaires/{id_commentaire}", response_model=CommentaireResponse, tags=["Commentaire"], status_code=status.HTTP_200_OK, summary="modification partielle.")
 async def patch_item(commentaire_id : int, commentUpdate : CommentaireUpdate):
+    db: Session = Depends(get_db)
+    db_commentaire = db.get(Commentaire, commentaire_id)
     client_comment = CommentaireResponse(id = commentaire_id,**commentUpdate.dict())
-    
-    for comment in TableCommentaire:
-        if comment.id_commentaire == commentaire_id:
-            comment.copy(update=client_comment)
-            return comment
-    raise HTTPException(status_code=404, detail="Comment not found")
+    if db_commentaire:
+        db_commentaire.copy(update=client_comment)
+        return db_commentaire
+    else : 
+        raise HTTPException(status_code=404, detail="Comment not found")
 
-@app.delete("/commentaires/{id_commentaire}", response_model = dict, tags=["Commentaires"], status_code=status.HTTP_200_OK, summary = "suppression d'une occurence précise")
+@app.delete("/commentaires/{id_commentaire}", response_model = dict, tags=["Commentaires"], status_code=status.HTTP_200_OK, summary = "suppression d'une occurence précise.")
 async def delete_item(commentaire_id : int):
-    for comment in TableCommentaire:
-        if comment.id_commentaire == commentaire_id:
-            TableCommentaire.remove(comment)
-            return {"Message" : "Commentaire supprimé"}
-    raise HTTPException(status_code=404, detail="Comment not found")
+    db: Session = Depends(get_db)
+    db_commentaire = db.get(Commentaire, commentaire_id)
+    if db_commentaire:
+            db.delete(db_commentaire)
+            db.commit()
+            return {"Message " : "Commentaire supprimé."}
+    else : 
+        raise HTTPException(status_code=404, detail="Comment not found.")
