54fe8e922c5caf96a037a84bb15ddd16c7cb530c
Guillaume Lemaitre <guillaume.lemaitre1@gmail.com>
2023-11-28 12:13:12 +0000

    add some fix to client_router.py and add TestPatchClient() to client_test.py

--- tests/client_test.py
+++ tests/client_test.py
@@ -3,6 +3,8 @@ import unittest
 from fastapi import status
 from fastapi.testclient import TestClient
 
+# cmd line to start the test: python -m unittest tests/client_test.py
+
 client = TestClient(app)
 valid_id_client = 2
 invalid_id_client = 99999
@@ -13,11 +15,27 @@ class TestReadClient(unittest.TestCase):
         response = client.get(f"/client/{valid_id_client}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_read_client_not_found(self):
-    #     # the id client is not valid
-    #     response = client.get(f"/client/{invalid_id_client}")
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_read_client_not_found(self):
+        # the id client is not valid
+        response = client.get(f"/client/{invalid_id_client}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
     
-if __name__ == '__main__':
-    unittest.main()
+class TestPatchClient(unittest.TestCase):
+
+    def test_patch_client_success(self):
+        response = client.patch(f"/client/{valid_id_client}", json={"nom_client": "(§è!çàç!è§((§è!ç)))"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data["nom_client"], "(§è!çàç!è§((§è!ç)))")
+
+    def test_patch_client_empty(self):
+        response = client.patch(f"/client/{valid_id_client}", json={"nom_client": ""})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data["nom_client"], "")
+
+    def test_patch_client_not_found(self):
+        # test with an invalid id client
+        response = client.patch(f"/client/{invalid_id_client}", json={"nom_client": "nomclient"})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         
