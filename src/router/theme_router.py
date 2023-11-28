f5b921d487115ead0723364fed801d7fcd22e13a
Benjamin Hadjaz <146709484+0bshidian@users.noreply.github.com>
2023-11-28 15:06:40 +0100

    modification theme ouvrage

--- src/router/theme_router.py
+++ src/router/theme_router.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, APIRouter, HTTPException, status, Depends
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 from config.connexion import get_db
 from src.models.theme import Theme
@@ -36,7 +35,7 @@ async def read_themes(db: Session = Depends(get_db)):
         db (Session, optional): a ne pas renseigner par l'utilisateur, est la connexion a la bdd | Defaults to Depends(get_db).
 
     Returns:
-        La liste de tous les themes 
+        La liste de tout les themes 
     """
     themes = db.query(Theme).all()
     return themes
@@ -65,21 +64,7 @@ async def read_theme(theme_id : int, db: Session = Depends(get_db)):
 
 #PUT : modification complete d'un theme     
 @theme_router.put("/themes/{id_theme}", response_model=ThemeResponse, tags=["Theme"], status_code=status.HTTP_200_OK, summary="modification d'un theme")
-async def update_theme(theme_id : int, themeUpdate : ThemeCreate, db: Session = Depends(get_db)):
-    """
-        Modification complete d'un theme 
-
-    Args:
-        theme_id (int): l'idée du theme recherché
-        themeUpdate (ThemeCreate): les infos modifiés du theme
-        db (Session, optional): a ne pas renseigner par l'utilisateur, est la connexion a la bdd | Defaults to Depends(get_db).
-
-    Raises:
-        HTTPException: renvoie un message si le theme à modifier n'existe pas dans la bdd
-
-    Returns:
-        Renvoie le theme modifié
-    """
+async def update_theme(theme_id : int, themeUpdate : ThemeUpdate, db: Session = Depends(get_db)):
     db_theme = db.get(Theme, theme_id)
     if db_theme:
         for key, value in themeUpdate.dict().items():
@@ -89,54 +74,9 @@ async def update_theme(theme_id : int, themeUpdate : ThemeCreate, db: Session = 
         return db_theme
     else: 
         raise HTTPException(status_code=404, detail="Theme not found.")
-
-#PATCH : la modification partielle d'un theme /!\ pour l'instant Theme ne contient qu'un attribut, mais pour l'avenir si une modification de la dbb ajouter plus de champ au theme
-@theme_router.patch("/theme/{id_theme}", response_model=ThemeResponse, tags=["Theme"], status_code=status.HTTP_200_OK, summary="modification partielle d'un theme")
-async def patch_theme(theme_id : int, themeUpdate : ThemeUpdate, db: Session = Depends(get_db)):
-    """
-        Modification partielle du theme
-
-    Args:
-        theme_id (int): l'id du theme recherché
-        themeUpdate (ThemeUpdate): les infos modifiés du theme, permet de ne rien rensiegner
-        db (Session, optional): a ne pas renseigner par l'utilisateur, est la connexion a la bdd | Defaults to Depends(get_db).
-
-
-    Raises:
-        HTTPException: renvoie un message si le theme à modifier n'existe pas dans la bdd
-
-    Returns:
-        Renvoie le theme avec les modifications effectués
-    """
-    db_theme = db.get(Theme, theme_id)
-    updated_theme = themeUpdate.dict(exclude_unset=True)
-    if db_theme:
-        for key, value in updated_theme.items():
-            if hasattr(db_theme, key):
-                setattr(db_theme, key, value)
-        db.commit()
-        return db_theme
-    else : 
-        raise HTTPException(status_code=404, detail="Comment not found")
-
     
-
-#DELETE : la suppression d'un theme    
 @theme_router.delete("/themes/{id_theme}", response_model = dict, tags=["Theme"], status_code=status.HTTP_200_OK, summary = "suppression d'une occurence précise.")
 async def delete_theme(theme_id : int, db: Session = Depends(get_db)):
-    """
-        La suppression d'un theme
-
-    Args:
-        theme_id (int): l'id du theme a supprimer
-        db (Session, optional): a ne pas renseigner par l'utilisateur, est la connexion a la bdd | Defaults to Depends(get_db).
-
-    Raises:
-        HTTPException: renvoie un message si le theme a supprimer n'existe pas dans la bdd
-
-    Returns:
-        Retourne un message confirmant la suppressin d'un theme 
-    """
     db_theme = db.get(Theme, theme_id)
     if db_theme:
             db.delete(db_theme)
